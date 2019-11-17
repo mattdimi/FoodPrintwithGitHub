a776fae8bab08bdd08a38ea25114b63857383dfe
mathdimi <mathieudimitriades@hotmail.com>
2019-11-18 00:24:00 +0100

    Version 1.003

---
+++ app/src/main/java/com/example/foodprint/Vegetable.java
@@ -0,0 +1,16 @@
+package com.example.foodprint;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public class Vegetable {
+    int[] mois;
+    String nom;
+    int empreinte_carbone;
+    ImageView imageView;
+
+
+    public Vegetable(Context context) {
+
+    }
+}
