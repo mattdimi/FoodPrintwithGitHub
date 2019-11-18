d83223e7ed1dd9f302c8f4671ee58cbbca642749
Frinzi Toscane <toscane.frinzi@student-cs.fr>
2019-11-18 10:51:41 +0100

    Version 1.012

---
+++ app/src/main/java/com/example/foodprint/FicheProduitActivity.java
@@ -0,0 +1,34 @@
+package com.example.foodprint;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FicheProduitActivity extends AppCompatActivity {
+
+
+    public Vegetable ProduitChoisi;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fiche_produit);
+
+
+
+
+
+    }
+
+    public void AjouterListe(View view) {
+
+    }
+
+    public void RetourGrid(View view) {
+
+
+    }
+}
