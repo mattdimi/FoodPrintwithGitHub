d028346f67d66d3b949ef1e7046fc910aaeb811d
Frinzi Toscane <toscane.frinzi@student-cs.fr>
2019-11-18 17:07:57 +0100

    version 1.013

--- app/src/main/java/com/example/foodprint/ListeVegetablesActivity.java
+++ app/src/main/java/com/example/foodprint/ListeVegetablesActivity.java
@@ -10,5 +10,7 @@ public class ListeVegetablesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_vegetables);
+
+        //MASTER
     }
 }
