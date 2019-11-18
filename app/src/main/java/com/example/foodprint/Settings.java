70f5c9b943c6b5a9b24c81fc8d727cbc6157d6c2
Frinzi Toscane <toscane.frinzi@student-cs.fr>
2019-11-18 09:57:51 +0100

    Version 1.007

--- app/src/main/java/com/example/foodprint/Settings.java
+++ app/src/main/java/com/example/foodprint/Settings.java
@@ -10,5 +10,6 @@ public class Settings extends MotherActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
 }
--- app/src/main/java/com/example/foodprint/Vegetable.java
+++ app/src/main/java/com/example/foodprint/Vegetable.java
@@ -6,15 +6,58 @@ import android.widget.ImageView;
 
 import androidx.appcompat.widget.AppCompatButton;
 
-public class Vegetable extends AppCompatButton{
+import java.util.Vector;
+
+public class
+Vegetable {
     int[] mois;
+
+
     String nom;
     int empreinte_carbone;
     ImageView imageView;
 
 
-    public Vegetable(Context context) {
-        super(context);
+    public Vegetable(int[] Mois, String Nom, int Empreinte_carbone, ImageView ImageView){
+        mois = Mois;
+        nom = Nom;
+        empreinte_carbone = Empreinte_carbone;
+        imageView = ImageView;
 
     }
+
+
+    public int[] getMois() {
+        return mois;
+    }
+
+    public void setMois(int[] mois) {
+        this.mois = mois;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEmpreinte_carbone() {
+        return empreinte_carbone;
+    }
+
+    public void setEmpreinte_carbone(int empreinte_carbone) {
+        this.empreinte_carbone = empreinte_carbone;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+
 }
